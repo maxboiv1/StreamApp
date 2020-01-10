04fb891743fa720cc0a44892d79a0f92d8dabba3
Maxime <maxime.boivin@ecole-isitech.fr>
2020-01-10 11:13:50 +0100

    mo

--- screens/MovieScreen.js
+++ screens/MovieScreen.js
@@ -1,33 +1,70 @@
 import React from 'react';
-import {ScrollView, StyleSheet, View, Dimensions,TouchableWithoutFeedback, Image} from 'react-native';
-import {Block, Button, Card, Theme, Text} from 'galio-framework';
+import {ScrollView, StyleSheet, View, Image} from 'react-native';
+import {Block, Button, Theme, Text} from 'galio-framework';
 
 class MovieScreen extends React.Component {
     render() {
-        const { navigation, product, horizontal, full, style, priceColor, imageStyle } = this.props;
-        const imageStyles = [styles.image, full ? styles.fullImage : styles.horizontalImage, imageStyle];
+
+        const {navigation} = this.props;
 
         return (
-            <Block row={horizontal} card flex style={[styles.product, styles.shadow, style]}>
-                <TouchableWithoutFeedback onPress={() => navigation.navigate('Pro', { product: product })}>
-                    <Block flex style={[styles.imageContainer, styles.shadow]}>
-                        <Image source={{ uri: 'http://hdqwalls.com/wallpapers/6-underground-4k-hj.jpg' }} style={imageStyles} />
+            <ScrollView>
+                <Image
+                    style={styles.bandeau}
+                    source={require('../assets/images/bandeauapp.png')}
+                />
+
+                <Block style={styles.container}>
+                    <Image
+                        style={styles.image}
+                        source={{uri: navigation.getParam('film').affiche}}
+                    />
+                    <Block  style={styles.infos}>
+                        <Text style={styles.titre}>{navigation.getParam('film').title}</Text>
+                        <Text>Date de sortie: {navigation.getParam('film').datesortie}</Text>
+                        <Text>Réalisateur: {navigation.getParam('film').real}</Text>
+                        <Text>Acteurs: {navigation.getParam('film').acteurs}</Text>
+                        <Text>Style: {navigation.getParam('film').style}</Text>
+                        <Text>{navigation.getParam('film').caption}</Text>
                     </Block>
-                </TouchableWithoutFeedback>
-                <TouchableWithoutFeedback onPress={() => navigation.navigate('Pro', { product: product })}>
-                    <Block flex space="between" style={styles.productDescription}>
-                        <Text size={14} style={styles.productTitle}>{navigation.getParam('film').title}</Text>
-                        <Text size={12} muted={!priceColor} color={priceColor}>veR</Text>
-                    </Block>
-                </TouchableWithoutFeedback>
-            </Block>
+                </Block>
+                <Text style={styles.synopsis}>Synopsis: {navigation.getParam('film').desc}</Text>
+            </ScrollView>
         );
     }
 }
 
 MovieScreen.navigationOptions = {
-    title: '{navigation.getParam(\'film\').title}',
+    title: 'Film',
 };
+
+const styles = StyleSheet.create({
+    bandeau: {
+        width: 375,
+        height: 100
+    },
+    container: {
+        margin: 10,
+        marginBottom: 0,
+        flexDirection: 'row',
+    },
+    image: {
+        width: 125,
+        height: 175
+    },
+    synopsis: {
+        margin: 10,
+    },
+    infos: {
+        marginTop: 0,
+        margin: 10,
+        marginRight: 10,
+    },
+    titre: {
+        fontWeight: 'bold',
+        marginBottom: 10,
+    },
+});
 
 
 export default MovieScreen;
